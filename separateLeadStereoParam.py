bc81f2304eb91e881fe72560ef08da4001168df1
Jean-Louis Durrieu <jean-louis@durrieu.ch>
2012-08-10 09:07:03 +0200

    dealing with issue #1
    https://github.com/wslihgt/separateLeadStereo/issues/1

--- separateLeadStereoParam.py
+++ separateLeadStereoParam.py
@@ -469,7 +469,7 @@ def generateHannBasis(numberFrequencyBins, sizeOfFourier, Fs, \
     return WGAMMA
 
 # MAIN FUNCTION, FOR DEFAULT BEHAVIOUR IF THE SCRIPT IS "LAUNCHED"
-def main(options, args):
+def main():
     import optparse
     
     usage = "usage: %prog [options] inputAudioFile"
